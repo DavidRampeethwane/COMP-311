44ea871711d082d2d6ed7300404de2ac5ab90cd8
David <rd24019720@biust.ac.bw>
2026-09-11 17:05:30 +0200

    Add Question3.java

---
+++ Lab 3/Question3.java
@@ -0,0 +1,10 @@
+public class Question3 {
+    public static void main(String[] args) {
+        int num = 2;                // initialize the variable num to 2, which is the first even number
+
+        while (num <= 50) {         // while loop that continues as long as num is less than or equal to 50
+            System.out.println(num);// print the current even number
+            num += 2;               // increment num by 2 to get the next even number
+        }
+    }
+}
