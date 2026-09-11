b9b34b12a94e8a063ae749c36dc4b0d756d4c528
David <rd24019720@biust.ac.bw>
2026-09-11 17:04:36 +0200

    add Question1.java

---
+++ Lab 3/Question1.java
@@ -0,0 +1,12 @@
+public class Question1 {
+    public static void main(String[] args) {           //main method that serves as the entry point of the program 
+        int num = 10;                                  //initialize the variable num to 10
+
+        while (num >= 0) {                             //while loop that continues as long as num is greater than or equal to 0
+            System.out.println("The number is " + num);//print the current value of num
+            num--;                                    //decrement num by 1
+        
+        }
+          System.out.println("listoff");               //print "listoff" after the loop ends
+    }
+}
