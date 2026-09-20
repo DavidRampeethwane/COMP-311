567ffdc7bbc10a118b2192bd9339bbfb0feffc68
David <rd24019720@biust.ac.bw>
2026-09-20 15:11:55 +0200

    Add Question4.java

---
+++ Lab 5/Question4.java
@@ -0,0 +1,12 @@
+// Demonstrates the use of getSummary method
+public class Question4 {
+
+    public static void main(String[] args) {
+
+        // Create a Book object
+        Book book = new Book("Java Programming", "John Smith", 250.50);
+
+        // Display book summary
+        System.out.println(book.getSummary());
+    }
+}
